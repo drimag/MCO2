5ba9c639990a737416ad8ee0b739ce99b0212a77
drimag <rafaelvedimagiba@gmail.com>
2024-10-19 13:04:41 +0800

    rounded pfp

--- app/src/main/java/com/mobdeve/s20/dimagiba/rafael/mco2/postViewHolder.kt
+++ app/src/main/java/com/mobdeve/s20/dimagiba/rafael/mco2/postViewHolder.kt
@@ -1,13 +1,22 @@
 package com.mobdeve.s20.dimagiba.rafael.mco2
 
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.mobdeve.s20.dimagiba.rafael.mco2.databinding.TreasurePostLayoutBinding
 
 class postViewHolder (private val viewBinding: TreasurePostLayoutBinding): RecyclerView.ViewHolder(viewBinding.root) {
 
     fun bindData(post: TreasureHunt) {
         this.viewBinding.usernameTv.text = post.poster.userName
-        post.imageId?.let { this.viewBinding.userImageIv.setImageResource(it) }
+
+        post.poster.profileImageId.let {
+            Glide.with(this.viewBinding.userImageIv)
+                .load(it)
+                .circleCrop()
+                .into(this.viewBinding.userImageIv)
+        }
+
+        post.imageId?.let { this.viewBinding.treasureImageIv.setImageResource(it) }
         this.viewBinding.dateTv.text = post.date.toStringNoYear()
         this.viewBinding.descriptionTv.text = post.description
         this.viewBinding.participantsTv.text = "${post.participants.size} Pirates"
