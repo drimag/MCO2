fa71594c57c7d53a67b980d7d6d8f020c042e6ae
drimag <rafaelvedimagiba@gmail.com>
2024-10-19 10:45:45 +0800

    added date to posts

--- app/src/main/java/com/mobdeve/s20/dimagiba/rafael/mco2/TreasureHunt.kt
+++ app/src/main/java/com/mobdeve/s20/dimagiba/rafael/mco2/TreasureHunt.kt
@@ -7,15 +7,18 @@ class TreasureHunt {
         private set
     var poster: User
         private set
+    var date: CustomDate
+        private set
     var participants: MutableList<User> = mutableListOf()
         private set
     var winners: MutableList<User> = mutableListOf()
         private set
 
-    constructor(description: String, imageId: Int, poster: User) {
+    constructor(description: String, imageId: Int, poster: User, date: CustomDate) {
         this.description = description
         this.imageId = imageId
         this.poster = poster
+        this.date = date
     }
 
     fun addWinner(user: User) {
