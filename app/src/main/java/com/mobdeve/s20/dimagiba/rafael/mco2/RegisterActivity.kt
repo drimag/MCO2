bdebb4a6eb095181a0df04591a1502272e6813da
francizesperanza <franciz_esperanza@dlsu.edu.ph>
2024-10-19 12:18:21 +0800

    Added register layout and activity, to login and to register works

--- app/src/main/java/com/mobdeve/s20/dimagiba/rafael/mco2/RegisterActivity.kt
+++ app/src/main/java/com/mobdeve/s20/dimagiba/rafael/mco2/RegisterActivity.kt
@@ -8,22 +8,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
-
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val toLoginBtn = findViewById<Button>(R.id.to_login_bt)
 
-        val toRegisterBtn = findViewById<Button>(R.id.to_register_bt)
-
-        toRegisterBtn.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
+        toLoginBtn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
     }
